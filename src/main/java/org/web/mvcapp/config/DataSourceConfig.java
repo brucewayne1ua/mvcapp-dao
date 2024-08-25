559cf548e303f3abe73435d7e8c1ef318b80d305
Rustam Ohli <adamsonrustam@gmail.com>
2024-08-26 00:56:38 +0300

    Add invisible of DB

--- src/main/java/org/web/mvcapp/config/DataSourceConfig.java
+++ src/main/java/org/web/mvcapp/config/DataSourceConfig.java
@@ -2,20 +2,33 @@ package org.web.mvcapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class DataSourceConfig {
+
+    private final Environment environment;
+
+    public DataSourceConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("rustam2005");
+
+
+          DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("db.driver")));
+        dataSource.setUrl(Objects.requireNonNull(environment.getProperty("db.url")));
+        dataSource.setUsername(Objects.requireNonNull(environment.getProperty("db.username")));
+        dataSource.setPassword(Objects.requireNonNull(environment.getProperty("db.password")));
 
         return dataSource;
     }
